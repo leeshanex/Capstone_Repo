4b462e76f5dbdc2425d5b159c99053f9d0e118b7
leeshanex <leeshanex@outlook.com>
2020-06-24 02:49:57 -0700

    Created starter for API keys - need API keys for each APIs using

---
+++ APIKeys/APIKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone_Proj.APIKeys
+{
+    public class APIKeys
+    {
+        public static readonly string OpenCurrentWeatherKey = "";
+        public static readonly string OpenCurrentLocationKey = "";
+        public static readonly string OpenSchedulerKey = "";
+
+    }
+}
