11c3604087f9d43f777cd31bcbaf385fd8538e8b
leeshanex <leeshanex@outlook.com>
2020-06-24 02:46:13 -0700

    Added starter code for service class for Scheduler API

---
+++ Services/GetScheduleService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone_Proj.Services
+{
+    public class GetScheduleService
+    {
+        public GetScheduleService()
+        {
+
+        }
+        //public async Task<> GetCurrentSchedule()
+        //{
+        //    HttpRequestMessage request = new HttpRequestMessage();
+        //    string url = $"";
+        //    HttpClient client = new HttpClient();
+        //    client.DefaultRequestHeaders.Add("");
+        //    client.DefaultRequestHeaders.Add("", APIKeys.OpenMusicSearchKey);
+
+        //    HttpResponseMessage response = await client.GetAsync(url);
+
+        //    if (response.IsSuccessStatusCode)
+        //    {
+        //        string json = await response.Content.ReadAsStringAsync();
+        //          = JsonConvert.DeserializeObject<>(json);
+        //        return ();
+        //    }
+        //    return null;
+        //}
+    }
+}
